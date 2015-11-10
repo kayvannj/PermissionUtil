f7bc82acab13c15c63686c4e6bd2c7f13ccd9573
Kayvan <kayvan.najafzadeh@gmail.com>
2015-11-10 09:01:00 -0800

    adding documentation and lowering minSdk closes #3

---
+++ PermissionUtils/src/main/java/com/github/kayvannj/permission_utils/Func3.java
@@ -0,0 +1,8 @@
+package com.github.kayvannj.permission_utils;
+
+/**
+ * Created by kayvan on 11/10/15.
+ */
+abstract public class Func3 {
+    protected abstract void call(String permissionName);
+}
--- PermissionUtils/src/main/java/com/github/kayvannj/permission_utils/PermissionUtil.java
+++ PermissionUtils/src/main/java/com/github/kayvannj/permission_utils/PermissionUtil.java
@@ -1,7 +1,6 @@
 package com.github.kayvannj.permission_utils;
 
 import android.content.pm.PackageManager;
-import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
@@ -10,25 +9,29 @@ import android.util.Log;
 import java.util.ArrayList;
 
 /**
- *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p/>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p/>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  * <p/>
  * Created by kayvan on 10/26/15.
  */
 public class PermissionUtil {
 
-    public static final int REQ_CODE_ALL_GRANTED = -1;
-    private static final String TAG = PermissionUtil.class.getSimpleName();
     static private AppCompatActivity mAppCompatActivity;
-
 
     public static PermissionObject with(AppCompatActivity activity) {
         mAppCompatActivity = activity;
         return new PermissionObject();
     }
 
-    /**
-     * Gets all the needed permission names and creates a PermissionRequestObject accordingly
-     */
     public static class PermissionObject {
 
         public PermissionRequestObject request(String permissionName) {
@@ -46,22 +49,28 @@ public class PermissionUtil {
 
         private ArrayList<SinglePermission> mPermissionsWeDontHave;
         private int mRequestCode;
-        private Func mRationalFunc;
         private Func mGrantFunc;
         private Func mDenyFunc;
         private Func2 mResultFunc;
+        private Func3 mRationalFunc;
         private String[] mPermissionNames;
 
         public PermissionRequestObject(String[] permissionNames) {
             mPermissionNames = permissionNames;
         }
 
+        /**
+         * Execute the permission request with the given Request Code
+         *
+         * @param reqCode
+         *         a unique request code in your activity
+         */
         public PermissionRequestObject ask(int reqCode) {
             mRequestCode = reqCode;
             int length = mPermissionNames.length;
             mPermissionsWeDontHave = new ArrayList<>(length);
-            for (int i = 0; i < length; i++) {
-                mPermissionsWeDontHave.add(new SinglePermission(mPermissionNames[i]));
+            for (String mPermissionName : mPermissionNames) {
+                mPermissionsWeDontHave.add(new SinglePermission(mPermissionName));
             }
 
             if (needToAsk()) {
@@ -69,8 +78,7 @@ public class PermissionUtil {
                 ActivityCompat.requestPermissions(mAppCompatActivity, mPermissionNames, reqCode);
             } else {
                 Log.i(TAG, "No need to ask for permission");
-                if(mGrantFunc!=null)
-                    mGrantFunc.call();
+                if (mGrantFunc != null) mGrantFunc.call();
             }
             return this;
         }
@@ -96,25 +104,52 @@ public class PermissionUtil {
             return mPermissionsWeDontHave.size() != 0;
         }
 
-        @NonNull public PermissionRequestObject onRational(@NonNull Func rationalFunc) {
+        /**
+         * Called for the first denied permission if there is need to show the rational
+         */
+        public PermissionRequestObject onRational(Func3 rationalFunc) {
             mRationalFunc = rationalFunc;
             return this;
         }
 
-        @NonNull public PermissionRequestObject onAllGranted(@NonNull Func grantFunc) {
+        /**
+         * Called if all the permissions were granted
+         */
+        public PermissionRequestObject onAllGranted(Func grantFunc) {
             mGrantFunc = grantFunc;
             return this;
         }
 
-        @NonNull public PermissionRequestObject onAllDenied(@NonNull Func denyFunc) {
+        /**
+         * Called if there is at least one denied permission
+         */
+        public PermissionRequestObject onAnyDenied(Func denyFunc) {
             mDenyFunc = denyFunc;
             return this;
         }
-        @NonNull public PermissionRequestObject onResult(@NonNull Func2 resultFunc) {
+
+        /**
+         * Called with the original operands from {@link AppCompatActivity#onRequestPermissionsResult(int, String[], int[])
+         * onRequestPermissionsResult} for any result
+         */
+        public PermissionRequestObject onResult(Func2 resultFunc) {
             mResultFunc = resultFunc;
             return this;
         }
 
+        /**
+         * This Method should be called from {@link AppCompatActivity#onRequestPermissionsResult(int, String[], int[])
+         * onRequestPermissionsResult} with all the same incoming operands
+         * <pre>
+         * {@code
+         *
+         * public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+         *      if (mStoragePermissionRequest != null)
+         *          mStoragePermissionRequest.onRequestPermissionsResult(requestCode, permissions,grantResults);
+         * }
+         * }
+         * </pre>
+         */
         public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
             Log.i(TAG, String.format("ReqCode: %d, ResCode: %d, PermissionName: %s", requestCode, grantResults[0], permissions[0]));
 
@@ -130,13 +165,14 @@ public class PermissionUtil {
                         if (mPermissionsWeDontHave.get(i).isRationalNeeded()) {
                             if (mRationalFunc != null) {
                                 Log.i(TAG, "Calling Rational Func");
-                                mRationalFunc.call();
+                                mRationalFunc.call(mPermissionsWeDontHave.get(i).getPermissionName());
                             }
                         }
                         if (mDenyFunc != null) {
                             Log.i(TAG, "Calling Deny Func");
                             mDenyFunc.call();
                         } else Log.e(TAG, "NUll DENY FUNCTIONS");
+
                         // terminate if there is at least one deny
                         return;
                     }
