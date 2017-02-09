a94aac55d828ce20fadfb3377278ef5cdb516d7d
Kayvan <kayvan.najafzadeh@gmail.com>
2017-02-09 15:48:31 -0800

    fixed a possible crash

--- app/src/main/java/com/kayvan/permissionutil/MainActivity.java
+++ app/src/main/java/com/kayvan/permissionutil/MainActivity.java
@@ -16,72 +16,45 @@ import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final String WRITE_EXTERNAL_STORAGE = Manifest.permission.WRITE_EXTERNAL_STORAGE;
+    public static final String WRITE_CONTACTS = Manifest.permission.WRITE_CONTACTS;
     private static final int REQUEST_CODE_CONTACTS = 1;
     private static final int REQUEST_CODE_STORAGE = 2;
     private static final int REQUEST_CODE_BOTH = 3;
-    public static final String WRITE_EXTERNAL_STORAGE = Manifest.permission.WRITE_EXTERNAL_STORAGE;
-    public static final String WRITE_CONTACTS = Manifest.permission.WRITE_CONTACTS;
     @Bind(R.id.status) TextView mStatus;
-    private PermissionUtil.PermissionRequestObject mStoragePermissionRequest;
-    private PermissionUtil.PermissionRequestObject mContactsPermissionRequest;
     private PermissionUtil.PermissionRequestObject mBothPermissionRequest;
+    private PermissionUtil.PermissionRequestObject mContactsPermissionRequest;
+    private PermissionUtil.PermissionRequestObject mStoragePermissionRequest;
 
     @Override protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
     }
-    @OnClick(R.id.storage_check) public void onCheckStoragePermissionClick(){
+
+    @OnClick(R.id.storage_check) public void onCheckStoragePermissionClick() {
         boolean hasStoragePermission = PermissionUtil.with(this).has(WRITE_EXTERNAL_STORAGE);
-        updateStatus(hasStoragePermission?"Has Storage permission":"Doesn't have Storage permission");
+        updateStatus(hasStoragePermission ? "Has Storage permission" : "Doesn't have Storage permission");
     }
-    @OnClick(R.id.contacts_check) public void onCheckContactsPermissionClick(){
+
+    private void updateStatus(String s) {mStatus.setText(String.format("> %s\n", s) + mStatus.getText().toString());}
+
+    @OnClick(R.id.contacts_check) public void onCheckContactsPermissionClick() {
         boolean hasContactsPermission = PermissionUtil.with(this).has(WRITE_CONTACTS);
-        updateStatus(hasContactsPermission?"Has Contacts permission":"Doesn't have Contacts permission");
+        updateStatus(hasContactsPermission ? "Has Contacts permission" : "Doesn't have Contacts permission");
     }
 
     @OnClick(R.id.storage) public void onAskForStoragePermissionClick() {
-        mStoragePermissionRequest = PermissionUtil.with(this).request(WRITE_EXTERNAL_STORAGE).onAllGranted(
-                new Func() {
-                    @Override protected void call() {
-                        doOnPermissionGranted("Storage");
-                    }
-                }).onAnyDenied(
-                new Func() {
-                    @Override protected void call() {
-                        doOnPermissionDenied("Storage");
-                    }
-                }).ask(REQUEST_CODE_STORAGE);
+        mStoragePermissionRequest = PermissionUtil.with(this).request(WRITE_EXTERNAL_STORAGE).onAllGranted(new Func() {
+            @Override protected void call() {
+                doOnPermissionGranted("Storage");
+            }
+        }).onAnyDenied(new Func() {
+            @Override protected void call() {
+                doOnPermissionDenied("Storage");
+            }
+        }).ask(REQUEST_CODE_STORAGE);
 
-    }
-
-    @OnClick(R.id.both) public void onAskBothPermissionsClick() {
-        mBothPermissionRequest =
-                PermissionUtil.with(this).request(WRITE_EXTERNAL_STORAGE, WRITE_CONTACTS).onResult(
-                        new Func2() {
-                            @Override protected void call(int requestCode, String[] permissions, int[] grantResults) {
-                                for (int i = 0; i < permissions.length; i++) {
-                                    if (grantResults[i] == PackageManager.PERMISSION_GRANTED) doOnPermissionGranted(permissions[i]);
-                                    else doOnPermissionDenied(permissions[i]);
-                                }
-                            }
-                        }).ask(REQUEST_CODE_BOTH);
-
-    }
-
-    @OnClick(R.id.contacts) public void onAskForContactsPermissionClick() {
-        mContactsPermissionRequest = PermissionUtil.with(this).request(WRITE_CONTACTS);
-        mContactsPermissionRequest.onAllGranted(
-                new Func() {
-                    @Override protected void call() {
-                        doOnPermissionGranted("Contacts");
-                    }
-                }).onAnyDenied(
-                new Func() {
-                    @Override protected void call() {
-                        doOnPermissionDenied("Contacts");
-                    }
-                }).ask(REQUEST_CODE_CONTACTS);
     }
 
     private void doOnPermissionDenied(String permission) {
@@ -92,15 +65,46 @@ public class MainActivity extends AppCompatActivity {
         updateStatus(permission + " Permission Granted");
     }
 
-    private void updateStatus(String s) {mStatus.setText(String.format("> %s\n", s) + mStatus.getText().toString());}
+    @OnClick(R.id.both) public void onAskBothPermissionsClick() {
+        mBothPermissionRequest = PermissionUtil.with(this).request(WRITE_EXTERNAL_STORAGE, WRITE_CONTACTS).onResult(new Func2() {
+            @Override protected void call(int requestCode, String[] permissions, int[] grantResults) {
+                for (int i = 0; i < permissions.length; i++) {
+                    if (grantResults[i] == PackageManager.PERMISSION_GRANTED) {
+                        doOnPermissionGranted(permissions[i]);
+                    } else {
+                        doOnPermissionDenied(permissions[i]);
+                    }
+                }
+            }
+        }).ask(REQUEST_CODE_BOTH);
+
+    }
+
+    @OnClick(R.id.contacts) public void onAskForContactsPermissionClick() {
+        mContactsPermissionRequest = PermissionUtil.with(this).request(WRITE_CONTACTS);
+        mContactsPermissionRequest.onAllGranted(new Func() {
+            @Override protected void call() {
+                doOnPermissionGranted("Contacts");
+            }
+        }).onAnyDenied(new Func() {
+            @Override protected void call() {
+                doOnPermissionDenied("Contacts");
+            }
+        }).ask(REQUEST_CODE_CONTACTS);
+    }
 
     @Override public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
-        if (mStoragePermissionRequest != null) mStoragePermissionRequest.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (mStoragePermissionRequest != null) {
+            mStoragePermissionRequest.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
 
-        if (mContactsPermissionRequest != null)
+        if (mContactsPermissionRequest != null) {
             mContactsPermissionRequest.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
 
-        if (mBothPermissionRequest != null) mBothPermissionRequest.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (mBothPermissionRequest != null) {
+            mBothPermissionRequest.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
 }
